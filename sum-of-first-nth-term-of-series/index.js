aa779b44e1212f04d1504ac4d894e4d3ca9a5ed1
Tracy Adams <tracy@tracycodes.com>
2018-12-16 18:29:08 -0700

    0 and 1 tests pass

--- sum-of-first-nth-term-of-series/index.js
+++ sum-of-first-nth-term-of-series/index.js
@@ -2,4 +2,13 @@ function SeriesSum(n) {
   // Series will be something like this: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
   // The second place is n*2
   // Every place after the second is (n*2)+(n-2) I think?
+  switch (n) {
+    case 0:
+      return "0.00";
+    case 1:
+      return "1.00";
+  }
+  for (i = 1; i < n; i++) {}
 }
+
+module.exports = SeriesSum;
