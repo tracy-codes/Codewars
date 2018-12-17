d2eb9c4eda4bc25c6c5f39b851c18f2481baf1a1
Tracy Adams <tracy@tracycodes.com>
2018-12-16 18:22:38 -0700

    Function notes, might have figured out algo

---
+++ sum-of-first-nth-term-of-series/index.js
@@ -0,0 +1,5 @@
+function SeriesSum(n) {
+  // Series will be something like this: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+  // The second place is n*2
+  // Every place after the second is (n*2)+(n-2) I think?
+}
