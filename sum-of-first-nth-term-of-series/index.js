12afa9b7c292888071ccec685c8bdd3d8b718f6f
Tracy Adams <tracy@tracycodes.com>
2018-12-16 18:54:49 -0700

    Solved sum-of-first-nth-term-of-series

--- sum-of-first-nth-term-of-series/index.js
+++ sum-of-first-nth-term-of-series/index.js
@@ -1,14 +1,36 @@
 function SeriesSum(n) {
   // Series will be something like this: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
   // The second place is n*2
-  // Every place after the second is (n*2)+(n-2) I think?
+  // Every place after the second is 1/(n*2)+(n-2) I think?
+
+  // my solution
   switch (n) {
     case 0:
       return "0.00";
     case 1:
       return "1.00";
   }
-  for (i = 1; i < n; i++) {}
+  let total = 1;
+  for (let i = 1; i <= n; i++) {
+    let frac = 0.0;
+    if (i == 1) {
+      continue;
+    }
+    if (i == 2) {
+      frac = 1 / 4;
+    } else {
+      frac = 1 / (i * 2 + (i - 2));
+    }
+    total += frac;
+  }
+  return total.toFixed(2);
+
+  // best solution
+  //   for (var s = 0, i = 0; i < n; i++) {
+  //     s += 1 / (1 + i * 3)
+  //   }
+
+  //   return s.toFixed(2)
 }
 
 module.exports = SeriesSum;
--- sum-of-first-nth-term-of-series/index.test.js
+++ sum-of-first-nth-term-of-series/index.test.js
@@ -7,11 +7,11 @@ test("Should equal 1.00", () => {
   expect(SeriesSum(1)).toBe("1.00");
 });
 test("Should equal 1.25", () => {
-  expect(SeriesSum(2)).toBe("1.00");
+  expect(SeriesSum(2)).toBe("1.25");
 });
 test("Should equal 1.39", () => {
-  expect(SeriesSum(3)).toBe("1.00");
+  expect(SeriesSum(3)).toBe("1.39");
 });
 test("Should equal 1.49", () => {
-  expect(SeriesSum(4)).toBe("1.00");
+  expect(SeriesSum(4)).toBe("1.49");
 });
