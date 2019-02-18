39907775e3a5b43172646db06722e3bded0a0d61
Tracy <tracy@tracycodes.com>
2019-02-18 11:30:32 -0700

    Init num people on bus challenge

---
+++ num-of-people-in-bus/index.js
@@ -0,0 +1,9 @@
+// Bus stops is a list of arrays
+  // Each bus stop has two values (eg: [10, 5])
+  // The first value is how many people get on the bus
+  // The second value is how many peolpe get off the bus
+  // Final calculation is the final number of people on the bus after the last stop
+
+var number = function(busStops){
+  // Good Luck!
+}
